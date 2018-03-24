05730b059ca83ec422ad5e3307f1df219da6e1f2
LuanMontanari <luanmontanari@hotmail.com>
2018-03-23 22:47:34 -0300

    criando a camado dao do chamado

---
+++ dao/ChamadoDao.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace dao;
+
+require_once 'C:/wamp/www/sgd/util/Connection.php';
+require_once 'C:/wamp/www/sgd/model/Chamado.php';
+
+/**
+ * Description of ChamadoDao
+ *
+ * @author Luan Souza Montanari
+ */
+use PDO;
+use util\Connection;
+use model\Chamado;
+
+class ChamadoDao {
+
+    const TABLE_CHAMADO = 'chamado';
+
+    private $connection;
+
+    function __construct() {
+        $this->connection = Connection::getConnection();
+    }
+
+    public function inserir($chamado = null) {
+        $var = $this->connection->prepare("insert into " . self::TABLE_CHAMADO . "(id_requerente, data_emissao, desc_requerente) values(:id_requerente, :data_emissao :desc_requerente)");
+
+        $var->bindValue('id_requerente', $chamado->getNomeUsuario(), PDO::PARAM_INT);
+        $var->bindValue('data_emissao', $chamado->getEmailUsuario(), PDO::PARAM_STR);
+        $var->bindValue('desc_requerente', $chamado->getLoginUsuario(), PDO::PARAM_STR);
+
+        $this->connection->beginTransaction();
+        $var->execute();
+        $this->connection->commit();
+    }
+
+    public function listar() {
+        $var = $this->connection->prepare("select * from " . self::TABLE_CHAMADO);
+        $var->execute();
+        return $var->fetchAll();
+    }
+
+    public function delete($id) {
+        $var = $this->connection->prepare("delete from " . self::TABLE_CHAMADO . " where id = :id");
+
+        $var->bindParam('id', $id, PDO::PARAM_INT);
+        $this->connection->beginTransaction();
+        $var->execute();
+        $this->connection->commit();
+    }
+
+    public function find($id) {
+        $sel = $this->connection->prepare("select *from " . self::TABLE_CHAMADO . " where id = :id");
+        $sel->bindValue('id', $id, PDO::PARAM_INT);
+        $sel->execute();
+        $result = $sel->fetchAll();
+
+        if ($result) {
+            $reg = $result[0];
+            $chamado = new Chamado($reg['id'], $reg['id_requerente'], $reg['id_tecnico'], $reg['id_supervisor'], $reg['prioridade'], $reg['status'], $reg['data_emissao'], $reg['data_aceitacao'], $reg['data_conclusao'], $reg['desc_requerente'], $reg['desc_tecnico'], $reg['data_supervisor'], $reg['dependencia_externa']);
+            return $chamado;
+        } else {
+            return null;
+        }
+    }
+
+    public function alterar_usuario(Chamado $chamado = null) {
+        $var = $this->connection->prepare("update " .self::TABLE_CHAMADO .
+                " set desc_requerente = :desc_requerente where id = :id ");
+        $var->bindValue('id', $chamado->getIdChamado(), PDO::PARAM_INT);
+        $var->bindValue('desc_requerente', $chamado->getDescricaoRequerenteChamado(), PDO::PARAM_STR);
+        
+        $this->connection->beginTransaction();
+        $var->execute();
+        $this->connection->commit();
+    }
+
+    public function alterar_tecnico(Chamado $chamado = null) {
+        $var = $this->connection->prepare("update " .self::TABLE_CHAMADO .
+                " set status = :status, desc_tecnico = :desc_tecnico where id = :id ");
+        $var->bindValue('id', $chamado->getIdChamado(), PDO::PARAM_INT);
+        $var->bindValue('status', $chamado->getStatusChamado(), PDO::PARAM_STR);
+        $var->bindValue('desc_tecnico', $chamado->getDescricaoTecnicoChamado(), PDO::PARAM_STR);
+        
+        $this->connection->beginTransaction();
+        $var->execute();
+        $this->connection->commit();
+    }
+
+    public function alterar_supervisor(Chamado $chamado = null) {
+        
+    }
+
+    public function alterar_adiministardor(Chamado $chamado = null) {
+        $var = $this->connection->prepare("update " .self::TABLE_CHAMADO .
+                " set prioridade = :prioridade, status = :status, desc_supervisor = :desc_supervisor where id = :id ");
+        $var->bindValue('id', $chamado->getIdChamado(), PDO::PARAM_INT);
+        $var->bindValue('prioridade', $chamado->getPrioridadeChamado(), PDO::PARAM_STR);
+        $var->bindValue('status', $chamado->getStatusChamado(), PDO::PARAM_STR);
+        $var->bindValue('desc_tecnico', $chamado->getDescricaoTecnicoChamado(), PDO::PARAM_STR);
+        
+        $this->connection->beginTransaction();
+        $var->execute();
+        $this->connection->commit();
+    }
+
+}
